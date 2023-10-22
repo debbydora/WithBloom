2da9ff109724e96b03fa467535c3efd3357d59c4
debbydora <debbyglitz94@gmail.com>
2023-10-22 13:36:45 +0100

    Added functionality for the exchange rate calculator

--- src/hooks/useCoins.tsx
+++ src/hooks/useCoins.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const useCoins = () => {
   const [coinList, setCoinList] = useState<any>();
@@ -6,6 +7,11 @@ const useCoins = () => {
   const [selectedCoin, setSelectedCoin] = useState("");
   const [filteredCoinList, setFilteredCoinList] = useState<any>();
   const [loading, setLoading] = useState(false);
+
+  const [amount, setAmount] = useState<string>("");
+  const [sourceCurrency, setSourceCurrency] = useState<string>("");
+  const [targetCurrency, setTargetCurrency] = useState<string>("");
+  const [result, setResult] = useState<number>(0);
 
   const url = `${import.meta.env.VITE_BASEURL}/currency/rate`;
 
@@ -55,6 +61,55 @@ const useCoins = () => {
     setSelectedCoin(e.target.value);
   };
 
+  const handleCalculate = () => {
+    const numericAmount = Number(amount);
+
+    if (isNaN(numericAmount)) {
+      toast.info("Please enter a valid number for the amount", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setAmount("")
+      setResult(0);
+      return;
+    }
+
+    const rateA = coinList.find(
+      (coin: any) => coin.name === sourceCurrency
+    )?.rate;
+    const rateB = coinList.find(
+      (coin: any) => coin.name === targetCurrency
+    )?.rate;
+
+    if (rateA !== undefined && rateB !== undefined) {
+      const convertedAmount = (numericAmount / rateA) * rateB;
+      setResult(convertedAmount);
+    } else {
+      setResult(0);
+      toast.info("Invalid source or target currency", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+   const handleReset = () => {
+     setAmount("");
+     setSourceCurrency("");
+     setTargetCurrency("");
+     setResult(0);
+   };
+
   return {
     coinList,
     onChange,
@@ -63,6 +118,15 @@ const useCoins = () => {
     selectedCoin,
     filteredCoinList,
     loading,
+    setAmount,
+    setSourceCurrency,
+    setTargetCurrency,
+    result,
+    handleCalculate,
+    handleReset,
+    targetCurrency,
+    sourceCurrency,
+    amount
   };
 };
 
--- src/pages/exchangeRate/ExchangeRate.tsx
+++ src/pages/exchangeRate/ExchangeRate.tsx
@@ -1,10 +1,105 @@
+import useCoins from "../../hooks/useCoins";
+import Button from "../../components/Button";
 
-const ExchangeRate = () => {
+function ExchangeRate() {
+  const {
+    coinList,
+    setAmount,
+    setSourceCurrency,
+    setTargetCurrency,
+    targetCurrency,
+    sourceCurrency,
+    result,
+    handleCalculate,
+    handleReset,
+    amount,
+  } = useCoins();
+
   return (
-    <div>
-      <p>Hello exchange Rate</p>
+    <div className="relative">
+      <h1 className="text-lg font-semibold mt-4 md:mt-0">
+        Currency Conversion Calculator
+      </h1>
+      <div className="flex flex-col bg-white p-[30px] md:w-[50%] w-[90%]  rounded-3xl fixed top-1/2 left-[50%] md:left-[60%] transform -translate-x-1/2 -translate-y-1/2 shadow-cardColor border border-[#8a66c4] text-black">
+        <form className="flex flex-col gap-6">
+          <div className="flex  md:flex-row gap-4 flex-col-reverse justify-between">
+            <label className=" font-semibold">
+              Amount:
+              <input
+                type="text"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter an amount e.g 12"
+                className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-[#8a66c4] outline-none md:w-full mt-2"
+              />
+            </label>
+            <div className="font-extrabold">
+              Result:
+              <p className="mt-1 text-[#6232b0]">
+                {typeof result === "number" ? result.toFixed(2) : result}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex  md:flex-row gap-4 flex-col justify-between font-semibold">
+            <label>
+              Source Currency:
+              <div className="flex flex-col p-2 shadow-cardColor border border-[#8a66c4] rounded-lg bg-gray-50 focus:border-[#8a66c4] outline-none cursor-pointer text-gray-900 font-normal mt-2">
+                <select
+                  value={sourceCurrency}
+                  onChange={(e) => setSourceCurrency(e.target.value)}
+                  className="outline-none bg-gray-50 cursor-pointer"
+                >
+                  <option value="">Select source currency</option>
+                  {coinList !== undefined &&
+                    coinList?.map((coin: any) => (
+                      <option key={coin?.name} value={coin?.name}>
+                        {coin?.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </label>
+            <label>
+              Target Currency:
+              <div className="flex flex-col p-2 shadow-cardColor border border-[#8a66c4] rounded-lg bg-gray-50 focus:border-[#8a66c4] outline-none cursor-pointer text-gray-900 font-normal mt-2">
+                <select
+                  value={targetCurrency}
+                  onChange={(e) => setTargetCurrency(e.target.value)}
+                  className="outline-none bg-gray-50 cursor-pointer"
+                >
+                  <option value="">Select target currency</option>
+                  {coinList !== undefined &&
+                    coinList?.map((coin: any) => (
+                      <option key={coin?.name} value={coin?.name}>
+                        {coin?.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </label>
+          </div>
+
+          <div className="flex justify-between md:flex-row gap-4 flex-col">
+            <Button
+              title={"Convert"}
+              type="button"
+              ariaLabel="exchange calculator button"
+              className="bg-[#3c009d] text-white p-3 rounded-2xl mx-auto mt-4 w-[40%] hover:scale-[.98]  transition ease-in duration-150 hover:bg-[#6232b0]"
+              onClick={handleCalculate}
+            />
+            <Button
+              title={"Reset"}
+              type="button"
+              ariaLabel="Reset button"
+              className="bg-[#3c009d] text-white p-3 rounded-2xl mx-auto mt-4 w-[40%] hover:scale-[.98]  transition ease-in duration-150 hover:bg-[#6232b0]"
+              onClick={handleReset}
+            />
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExchangeRate
+export default ExchangeRate;
